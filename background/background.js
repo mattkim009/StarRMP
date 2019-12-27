90f40853202cfae1f77c02684d663fae53bae091
Tyler Chong <tcchong@hawaii.edu>
2019-12-26 17:58:21 -1000

    Working with STAR website

--- background/background.js
+++ background/background.js
@@ -4,62 +4,62 @@
 
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ color: '#3aa757' }, function () {
-    // console.log('The color is green.');
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: 'developer.chrome.com' },
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
-
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  // if(changeInfo && changeInfo.status == "complete"){
-  //     chrome.tabs.executeScript(tabId, {file: "jquery-3.1.1.min.js"}, function(){
-  //         chrome.tabs.executeScript(tabId, {file: "semantic.min.js"}, function(){
-  //           chrome.tabs.executeScript(tabId, {file: "content.js"});
-  //     });
-  // });
-});
+// chrome.runtime.onInstalled.addListener(function () {
+//   chrome.storage.sync.set({ color: '#3aa757' }, function () {
+//     // console.log('The color is green.');
+//   });
+//   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+//     chrome.declarativeContent.onPageChanged.addRules([{
+//       conditions: [new chrome.declarativeContent.PageStateMatcher({
+//         pageUrl: { hostEquals: 'developer.chrome.com' },
+//       })],
+//       actions: [new chrome.declarativeContent.ShowPageAction()]
+//     }]);
+//   });
+// });
 
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
     if (request.contentScriptQuery == "queryRatingsWithName") {
       var url = `https://www.ratemyprofessors.com/search.jsp?queryoption=HEADER&queryBy=teacherName&schoolName=University+Of+Hawaii&query=${request.name}`;
       $.ajax({
-        url: url
+        url: url,
+        timeout: 5000,
       }).done(function (result) {
         const page = $(result).find("li.listing.PROFESSOR > a")[0];
         if (page != undefined) {
           let href = page.href;
           href = href.slice(href.lastIndexOf('/'), href.length);
           let link = `https://www.ratemyprofessors.com` + href;
-          console.log(link);
           $.ajax({
-            url: link
+            url: link,
+            timeout: 5000,
           }).done(function (rmpResult) {
-            console.log('meow');
             //TODO: TBA and store prof page query data onto local variable or store google local storage
             //TODO: make HTML query searches more specific
             //TODO: cache
-            //TODO: style popup so it looks legit
             //TODO: Do name in RateMyProfessor have dashes
             var quality = $(rmpResult).find("div.RatingValue__Numerator-qw8sqy-2.gxuTRq").text();
             var number_of_ratings = $(rmpResult).find('div.RatingValue__NumRatings-qw8sqy-0.BDziL a').text().replace('ratings', '').trim();
             var tags = $(rmpResult).find('div.TeacherTags__TagsContainer-sc-16vmh1y-0.dbxJaW span').map((i, val) => $(val).text());
-            sendResponse(
-              { link,
-                quality,
-                number_of_ratings,
-                tags
-              });
+            if (!quality || !number_of_ratings) {
+              sendResponse({error: `Couldn't parse the information from this professor's website.`});
+            } else {
+              sendResponse(
+                { link,
+                  quality,
+                  number_of_ratings,
+                  tags
+                });
+            }
+          }).fail(function() {
+            sendResponse({error: `Couldn't parse the information from this professor's website.`});
           });
-        };
+        } else {
+          sendResponse({error: `Couldn't find "${request.name}".`});
+        }
+      }).fail(function() {
+        sendResponse({error: `Couldn't find "${request.name}".`});
       });
 
       return true;  // Will respond asynchronously.
--- content/content.js
+++ content/content.js
@@ -1,45 +1,215 @@
-console.log('Content Script Running!');
+console.log('StarRMP Running!');
 
 $(document).ready(function () {
-  $('.n-requirement-component.n-course-instructor p')
-    .popup({
-      position: 'top center',
-      // target   : '.test.image',
-      title: 'My favorite dog',
-      content: 'My favorite dog would like other dogs as much as themselves'
-      // popup: '#pop'
-    });
-  console.log($('.n-requirement-component.n-course-instructor p'));
-
-  $('.n-requirement-component.n-requirement-course-instructor p')
-    .popup({
-      position: 'top center',
-      // target   : '.test.image',
-      title: 'My favorite dog',
-      content: 'My favorite dog would like other dogs as much as themselves'
-      // popup: '#pop'
-    });
-  console.log($('.n-requirement-component.n-requirement-course-instructor p'));
-
   var popupLoading = '<div class="ui inline active loader indeterminate text centered tiny">Loading...</div>';
+
+  var intervalID = setInterval(function () {
+    if ($('.n-course').length > 2) {
+      $('.n-course-component.n-course-instructor')
+        .each(function () {
+          $(this).css('cursor', 'pointer');
+          const name = $(this).text().trim().replace(/ /g, ' ').split('\n')[0].split(' ').reverse().join(' ').replace(/-/g, ' ');
+          if (!name) {
+            $(this.popup({
+              position: 'left center',
+              exclusive: true,
+              hoverable: true,
+              html: `Invalid Search`,
+              delay: {
+                show: 400,
+                hide: 400
+              },
+            }))
+          } else {
+            $(this).popup({
+              position: 'left center',
+              exclusive: true,
+              hoverable: true,
+              html: popupLoading,
+              delay: {
+                show: 400,
+                hide: 400
+              },
+              onShow: function (el) { // load data (it could be called in an external function.)
+                var popup = this;
+                popup.html(popupLoading);
+                chrome.runtime.sendMessage({ contentScriptQuery: "queryRatingsWithName", name: name },
+                  response => {
+                    if (!response) {
+                      return popup.html(`Error!`);
+                    }
+                    if (response.error) {
+                      return popup.html(`${response.error}`);
+                    }
+                    var tags = ``;
+                    for (let i = 0; i < response.tags.length; i++) {
+                      tags += `<span class="card-tag">${response.tags[i]}</span>`
+                    }
+                    popup.html(`
+                  <div>
+                  <a href=${response.link} target="_blank">
+                  <article class="card assignment-card course-id-1">
+                    <header class="card-header card-background-color">
+                      <h2>${name}</h2>
+                    </header>
+                    <section class="card-body">
+                      <div class="card-info">
+                        <div class="card-info-element">
+                          <div class="card-info-description">
+                            Quality
+                          </div>
+                          <div class="card-info-value">
+                            ${response.quality}
+                          </div>
+                        </div>
+                        <div class="card-info-element">
+                          <div class="card-info-description">
+                            # Ratings
+                          </div>
+                          <div class="card-info-value">
+                            ${response.number_of_ratings}
+                          </div>
+                        </div>
+                      </div>
+                      <p class="card-body-delimiter"></p>
+                      <br />
+                      <p class="card-tags">
+                        ${tags}
+                      </p>
+                    </section>
+                  </article>
+                  </a>
+                  </div>
+                `);
+                  }
+                );
+              }
+
+            })
+          }
+        });
+      clearInterval(intervalID);
+    }
+  }, 250);
+
+
+
+  var intervalID2 = setInterval(function () {
+    if ($('#content').length) {
+      $('.n-requirement-component.n-requirement-course-instructor')
+        .each(function () {
+          $(this).css('cursor', 'pointer');
+          const name = $(this).text().trim().replace(/ /g, ' ').split('\n')[0].split(' ').reverse().join(' ').replace(/-/g, ' ');
+          if (!name) {
+            $(this.popup({
+              position: 'left center',
+              exclusive: true,
+              hoverable: true,
+              html: `Invalid Search`,
+              delay: {
+                show: 400,
+                hide: 400
+              },
+            }))
+          } else {
+            $(this).popup({
+              position: 'left center',
+              exclusive: true,
+              hoverable: true,
+              html: popupLoading,
+              delay: {
+                show: 400,
+                hide: 400
+              },
+              onShow: function (el) { // load data (it could be called in an external function.)
+                var popup = this;
+                popup.html(popupLoading);
+                chrome.runtime.sendMessage({ contentScriptQuery: "queryRatingsWithName", name: name },
+                  response => {
+                    if (!response) {
+                      return popup.html(`Error!`);
+                    }
+                    if (response.error) {
+                      return popup.html(`${response.error}`);
+                    }
+                    var tags = ``;
+                    for (let i = 0; i < response.tags.length; i++) {
+                      tags += `<span class="card-tag">${response.tags[i]}</span>`
+                    }
+                    popup.html(`
+                    <div>
+                    <a href=${response.link} target="_blank">
+                    <article class="card assignment-card course-id-1">
+                      <header class="card-header card-background-color">
+                        <h2>${name}</h2>
+                      </header>
+                      <section class="card-body">
+                        <div class="card-info">
+                          <div class="card-info-element">
+                            <div class="card-info-description">
+                              Quality
+                            </div>
+                            <div class="card-info-value">
+                              ${response.quality}
+                            </div>
+                          </div>
+                          <div class="card-info-element">
+                            <div class="card-info-description">
+                              # Ratings
+                            </div>
+                            <div class="card-info-value">
+                              ${response.number_of_ratings}
+                            </div>
+                          </div>
+                        </div>
+                        <p class="card-body-delimiter"></p>
+                        <br />
+                        <p class="card-tags">
+                          ${tags}
+                        </p>
+                      </section>
+                    </article>
+                    </a>
+                    </div>
+                  `);
+                  }
+                );
+              }
+
+            })
+          }
+        });
+      clearInterval(intervalID2);
+    }
+  },
+    500);
+
+
+
   $('table[class="listOfClasses"] tr td:nth-child(7) abbr')
     .each(function () {
-      const name = $(this).attr("title");
+      $(this).css('cursor', 'pointer');
+      const name = $(this).attr("title").replace(/-/g, ' ');
       $(this).popup({
-        position: 'top center',
+        position: 'bottom center',
         exclusive: true,
         hoverable: true,
         html: popupLoading,
         delay: {
-          show: 200,
-          hide: 40000
+          show: 400,
+          hide: 400
         },
         onShow: function (el) { // load data (it could be called in an external function.)
           var popup = this;
-          console.log(name);
           popup.html(popupLoading);
           chrome.runtime.sendMessage({ contentScriptQuery: "queryRatingsWithName", name: name },
             response => {
+              if (!response) {
+                return popup.html(`Error!`);
+              }
+              if (response.error) {
+                return popup.html(`${response.error}`);
+              }
               var tags = ``;
               for (let i = 0; i < response.tags.length; i++) {
                 tags += `<span class="card-tag">${response.tags[i]}</span>`
@@ -81,11 +251,7 @@ $(document).ready(function () {
               </div>
             `);
             }
-
-
-
           );
-
         }
       })
     });
